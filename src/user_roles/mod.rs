88661d84c6633fe01e815fd2d677bdff5981072f
Archives <archives@Archivess-MBP.domain.name>
2020-03-31 01:36:33 +0100

    all working with return json values

--- src/user_roles/mod.rs
+++ src/user_roles/mod.rs
@@ -1,18 +1,31 @@
 
-
+/// Defines the roles of a user and manages the storing and lookup of these roles.
 pub mod user_roles {
 
     mod redis_connection;
-    use std::any::type_name;
+    use serde_json::json;
 
+    /// Data that describes the result of reading or writing a role with a `jwt_token`.
     pub struct UserRole {
+        /// role of the user
         pub role: String,
+        /// if the read or write was successful or not
         pub success: bool,
+        /// error message if there's a failure in the reading or writing to redis
         pub message: String
     }
 
     impl UserRole {
 
+        /// Creates a new user role and inserts it into redis.
+        ///
+        ///  # Arguments
+        ///
+        /// * `jwt_token` - user jwt token that is bound to a role
+        /// * `role` - role that the user is assigned
+        ///
+        /// # Returns
+        /// The status of the write process to redis
         pub fn new(jwt_token: String, role: String) -> UserRole {
 
             let result: UserRole = match redis_connection::roles::insert_role(jwt_token, &role) {
@@ -22,6 +35,13 @@ pub mod user_roles {
             return result
         }
 
+        /// Gets a role from an existing `jwt_token` stored in redis.
+        ///
+        /// # Arguments
+        /// * `jwt_token` - an existing user jwt token that is bound to a role
+        ///
+        /// # returns
+        /// The status of the read process to redis
         pub fn get(jwt_token: String) -> UserRole {
 
             let result: UserRole = match redis_connection::roles::get_role(jwt_token) {
@@ -31,6 +51,22 @@ pub mod user_roles {
             return result
         }
 
-    }
+        /// Packs the data of the UserRole struct into json, and then a String for response.
+        ///
+        /// # Arguments
+        /// None
+        ///
+        /// # returns
+        /// packed UserRole data for hyper body in a hyper response
+        pub fn pack(self) -> String {
 
+            let json_buffer = json!({
+                "role": self.role,
+                "success": self.success,
+                "message": self.message
+            });
+
+            return serde_json::to_string(&json_buffer).unwrap()
+        }
+    }
 }
--- src/user_roles/user_roles/redis_connection.rs
+++ src/user_roles/user_roles/redis_connection.rs
@@ -1,9 +1,17 @@
 
-
+/// Module for getting and inserting roles in the redis cache.
 pub mod roles {
 
     use redis::{self, Commands, RedisResult};
 
+    /// Gets a role based on a ```jwt_token```
+    ///
+    /// # Arguments
+    ///
+    ///* `jwt_token` - A String struct that contains the ```jwt_token```
+    ///
+    /// # Returns
+    /// The role that's tied to the ```jwt_token```
     pub fn get_role(jwt_token: String) -> redis::RedisResult<String> {
         let client = redis::Client::open("redis://127.0.0.1:6379")?;
         let mut con = client.get_connection()?;
@@ -11,6 +19,12 @@ pub mod roles {
         return role
     }
 
+    /// Inserts a role that's indexed by a ```jwt_token```
+    ///
+    /// # Arguments
+    ///
+    /// * `jwt_token` - A String struct that contains the ```jwt_token```
+    /// * `role` - A String struct that contains the role of the user
     pub fn insert_role(jwt_token: String, role: &String) -> RedisResult<String> {
         let client = redis::Client::open("redis://127.0.0.1:6379")?;
         let mut con = client.get_connection()?;
